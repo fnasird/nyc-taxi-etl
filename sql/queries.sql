21b08bc2ba1d2e8cfa58730b864d1153c2b75332
fnasird <fnasirdeen@gmail.com>
2025-09-21 16:26:00 -0400

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,19 +1,24 @@
--- Top 10 busiest pickup hours
-SELECT EXTRACT(HOUR FROM pickup_datetime) AS hour, COUNT(*) AS trips
-FROM trips
+-- Busiest pickup hours
+SELECT 
+  EXTRACT(HOUR FROM tpep_pickup_datetime) AS hour,
+  COUNT(*) AS trip_count
+FROM `nyc-taxi-etl-472801.nyc_taxi_data.trips`
 GROUP BY hour
-ORDER BY trips DESC
+ORDER BY trip_count DESC
 LIMIT 10;
 
--- Average trip distance by pickup location
-SELECT pu_location_id, AVG(trip_distance) AS avg_distance
-FROM trips
-GROUP BY pu_location_id
-ORDER BY avg_distance DESC
-LIMIT 10;
+-- Count trips per borough
+SELECT z.Borough, COUNT(*) AS trip_count
+FROM `nyc-taxi-etl-472801.nyc_taxi_data.trips` t
+JOIN `nyc-taxi-etl-472801.nyc_taxi_data.zones` z
+  ON t.PULocationID = z.LocationID
+GROUP BY z.Borough
+ORDER BY trip_count DESC;
 
 -- Daily revenue
-SELECT DATE(pickup_datetime) AS day, SUM(total_amount) AS revenue
-FROM trips
+SELECT 
+  DATE(tpep_pickup_datetime) AS day,
+  SUM(total_amount) AS daily_revenue
+FROM `nyc-taxi-etl-472801.nyc_taxi_data.trips`
 GROUP BY day
 ORDER BY day;
